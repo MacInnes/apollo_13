101fb1ce8e9eed4438943724be6a3e1292f63656
MacInnes <califormula@gmail.com>
2018-07-06 11:14:48 -0600

    safety commit, about to break stuff

--- spec/features/user_visits_astronauts_page_spec.rb
+++ spec/features/user_visits_astronauts_page_spec.rb
@@ -10,12 +10,25 @@ describe 'visitor' do
       expect(page).to have_content("Age: #{astronaut_1.age}")
       expect(page).to have_content("Job: #{astronaut_1.job}")
     end
+
     it 'displays the average age of all astronauts' do
       astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: 'Commander')
       astronaut_2 = Astronaut.create(name: "Jim Lovell", age: 39, job: 'Commander')
 
       visit '/astronauts'
       expect(page).to have_content("Average age: 38")
+    end
+
+    it 'displays the space missions in alphabetical order for each astronaut' do
+      astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: 'Commander')
+      astronaut_2 = Astronaut.create(name: "Jim Lovell", age: 39, job: 'Commander')
+      astronaut_1.space_missions.create(name: "Apollo 13")
+      astronaut_1.space_missions.create(name: "Capricorn 4")
+      astronaut_1.space_missions.create(name: "Gemini 7")
+
+      visit '/astronauts'
+      expect(page).to have_content("Space Missions: Apollo 13, Capricorn 4, Gemini 7")
+
     end
   end
 end
