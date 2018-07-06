34b67849fc25b51a2102588e1cdf4d0a8507e666
MacInnes <califormula@gmail.com>
2018-07-06 11:03:33 -0600

    first test passing

--- spec/features/user_visits_astronauts_page_spec.rb
+++ spec/features/user_visits_astronauts_page_spec.rb
@@ -6,7 +6,9 @@ describe 'visitor' do
       astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: 'Commander')
 
       visit '/astronauts'
-      expect(page).to have_content("Name: #{astronaut_1.name}", "Age: #{astronaut_1.age}", "Job: #{astronaut_1.job}")
+      expect(page).to have_content("Name: #{astronaut_1.name}")
+      expect(page).to have_content("Age: #{astronaut_1.age}")
+      expect(page).to have_content("Job: #{astronaut_1.job}")
     end
   end
 end
