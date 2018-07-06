e7617a031bda58bbec2d17959b9f3eb7dac1285e
MacInnes <califormula@gmail.com>
2018-07-06 11:06:50 -0600

    second test passing

--- spec/features/user_visits_astronauts_page_spec.rb
+++ spec/features/user_visits_astronauts_page_spec.rb
@@ -10,5 +10,12 @@ describe 'visitor' do
       expect(page).to have_content("Age: #{astronaut_1.age}")
       expect(page).to have_content("Job: #{astronaut_1.job}")
     end
+    it 'displays the average age of all astronauts' do
+      astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: 'Commander')
+      astronaut_2 = Astronaut.create(name: "Jim Lovell", age: 39, job: 'Commander')
+
+      visit '/astronauts'
+      expect(page).to have_content("Average age: 38")
+    end
   end
 end
